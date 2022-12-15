608f9d7b1386665745212de56a57dc525125e2aa
Maria <mtens17@gmail.com>
2022-12-15 16:11:27 +0000

    Add firebase, config

--- src/AuthDemoStart.tsx
+++ src/AuthDemoStart.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import React, { useState } from "react";
+import {auth} from "./configureFirebase";
+import {googleAuthProvider} from './configureFirebase'
+import {signInWithPopup} from 'firebase/auth'
 
 export function AuthDemoStart(): JSX.Element {
     const [lastAPIReply, setLastAPIReply] = useState<string>("");
-
+    const [user, setUser] = useState()
 
     async function handleFetchTimeClicked() {
         const reply = await axios.get("http://localhost:4000/");
@@ -15,14 +18,17 @@ export function AuthDemoStart(): JSX.Element {
         const reply = await axios.get("http://localhost:4000/wisdom");
         setLastAPIReply(reply.data);
     }
+    // const user = //? how to paste the user object 
 
     return (
         <div>
+            <pre>{JSON.stringify(auth)}</pre>
             <h2>Auth Demo</h2>
 
-            <button onClick={() => alert("not implemented")}>Sign in</button>
-            <button onClick={() => alert("not implemented")}>Sign out</button>
-
+            <button onClick={async ()=> {await signInWithPopup(auth, googleAuthProvider).then((result)=> console.log(result))}}>Sign in</button>
+            <button onClick={() => {auth.signOut()}}>Sign out</button>
+            //TODO: ; setUser() in both sign in and out 
+            {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
             <hr />
             <h3>Talk to the API</h3>
             <button onClick={handleFetchTimeClicked}>Fetch Time</button>
