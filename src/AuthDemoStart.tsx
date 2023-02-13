b8addc7635566d1f3eb5d49f2900a3fb553839e3
mariatens <mtens17@gmail.com>
2023-02-13 16:53:27 +0000

    First draft

--- src/AuthDemoStart.tsx
+++ src/AuthDemoStart.tsx
@@ -2,11 +2,11 @@ import axios from "axios";
 import React, { useState } from "react";
 import {auth} from "./configureFirebase";
 import {googleAuthProvider} from './configureFirebase'
-import {signInWithPopup} from 'firebase/auth'
+import {signInWithPopup, User} from 'firebase/auth'
 
 export function AuthDemoStart(): JSX.Element {
     const [lastAPIReply, setLastAPIReply] = useState<string>("");
-    const [user, setUser] = useState()
+    const [user, setUser] = useState<User|null>()
 
     async function handleFetchTimeClicked() {
         const reply = await axios.get("http://localhost:4000/");
@@ -14,21 +14,34 @@ export function AuthDemoStart(): JSX.Element {
     }
 
     async function handleFetchWisdomClicked() {
-        //This SHOULD be hard to get, eventually.
-        const reply = await axios.get("http://localhost:4000/wisdom");
+        if (!user){return}
+        const idToken = await user?.getIdToken()
+        const config = {headers: {"Authorization": "Bearer" + idToken}}
+        const reply = await axios.get("http://localhost:4000/wisdom", config);
         setLastAPIReply(reply.data);
     }
-    // const user = //? how to paste the user object 
+
+    const handleSignIn = async ()=> 
+    {
+        //TODO: Clear this state variable when log in doesn’t work.
+
+        const signInCredentials = await signInWithPopup(auth, googleAuthProvider)
+        const signedInUser = signInCredentials.user
+        setUser(signedInUser)
+    }
+
 
     return (
         <div>
             <pre>{JSON.stringify(auth)}</pre>
             <h2>Auth Demo</h2>
 
-            <button onClick={async ()=> {await signInWithPopup(auth, googleAuthProvider).then((result)=> console.log(result))}}>Sign in</button>
-            <button onClick={() => {auth.signOut()}}>Sign out</button>
-            //TODO: ; setUser() in both sign in and out 
-            {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
+            <button onClick={handleSignIn}>Sign in</button>
+            <button onClick={() => {auth.signOut(); setUser(null)}}>Sign out</button>
+        {/* //TODO:clear the user state variable when you sign out, too. */}
+            <div>{user?.displayName}
+            {user && user.photoURL && <img src = {user.photoURL} />}</div>
+           
             <hr />
             <h3>Talk to the API</h3>
             <button onClick={handleFetchTimeClicked}>Fetch Time</button>
